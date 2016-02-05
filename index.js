fea7edc78da361fde9684a8eef333b83377160a8
Luigi Poole <luigipoole@outlook.com>
2016-02-04 17:52:38 -0800

    Initial Commit :fireworks:

---
+++ index.js
@@ -0,0 +1,53 @@
+/**
+ * Module dependencies.
+ */
+import http from 'http';
+import app from './app';
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+server.on('error', error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error('Port ' + port + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error('Port ' + port + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+});
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+server.on('listening', () => console.log('Listening on port ' + server.address().port));
+
